7b95ac82b843d70b2372cb9dbf86c049cc47eedb
Jake Herrmann <herrmann.jake@protonmail.com>
2025-11-14 16:54:37 -0500

    Tweaked how scrape URL is obtained

--- src/main/java/com/jlh/json_scraper/RequestController.java
+++ src/main/java/com/jlh/json_scraper/RequestController.java
@@ -14,13 +14,11 @@ public class RequestController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private final String PORT = "8080/";
-
     @GetMapping("/**")
     public String index(HttpServletRequest request) {
         String url = request.getRequestURL().toString() + "?" +request.getQueryString();
         
-        url = url.substring(url.indexOf(PORT) + PORT.length());
+        url = url.substring(url.lastIndexOf("https://"));
         logger.trace(url);
 
         return ScraperController.getInstance().scrape(url);
